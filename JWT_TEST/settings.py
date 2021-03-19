8e47f3d3bbd7e0c05bb911c6e070111c4a11063e
Kate <you@example.com>
2021-03-19 13:24:14 +0300

    created serializers

--- JWT_TEST/settings.py
+++ JWT_TEST/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'psycopg2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
-    'JwtApp.apps.JwtappConfig'
+    'users.apps.UsersConfig',
+    'JwtApp.apps.JwtappConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
